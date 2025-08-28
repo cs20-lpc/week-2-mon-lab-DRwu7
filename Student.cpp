2707d776dc4e6368a250030996a19e863353dc3e
Chris <rjwu666@gmail.com>
2025-08-27 21:06:36 -0700

    this is my updated code

--- Student.cpp
+++ Student.cpp
@@ -1,39 +1,27 @@
-#include <iostream>
-#include <string>
 #include "Student.hpp"
+#include <iostream>
 using namespace std;
 
-// default constructor
 Student::Student() {
-    cout << "Student constructor called" << endl;
+    cout << "Student created" << endl;
 }
 
-// overloaded constructor
-Student::Student(string n, int a) : name(n), age(a) {
-    cout << "Overloaded constructor called" << endl;
-}
-
-// destructor
 Student::~Student() {
-    cout << "Student object destroyed!" << endl;
+    cout <<"Student deeeestroyed!"<< endl;
 }
 
-// setter for name
-void Student::setName(string n) {
+void Student::setName(const string& n) {
     name = n;
 }
 
-// setter for age
 void Student::setAge(int a) {
     age = a;
 }
 
-// getter for name
 string Student::getName() const {
     return name;
 }
 
-// getter for age
 int Student::getAge() const {
     return age;
 }
--- Student.hpp
+++ Student.hpp
@@ -1,23 +1,22 @@
 #ifndef STUDENT_HPP
 #define STUDENT_HPP
+
 #include <string>
 using namespace std;
+
 class Student {
-    private:
-        string name;
-        int age;
-    public:
-        // default constructor
-        Student();
-        // overloaded constructor
-        Student(string, int);
-        // destructor
-        ~Student();
-        // setters
-        void setName(string);
-        void setAge(int);
-        // getters
-        string getName() const;
-        int getAge() const;
+private:
+    string name;
+    int age;
+
+public:
+    Student();                    // default constructor
+    ~Student();                   // destructor
+
+    void setName(const string& n);
+    void setAge(int a);
+    string getName() const;
+    int getAge() const;
 };
+
 #endif
